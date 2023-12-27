01bf0f4da0554ab78d447241643670cd4992660d
Rei <rei@reiyua.lol>
2023-12-27 12:37:57 +0000

    Add sorry paragraph lmao

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ import { getFirestore,
         <Form>
           <Form.Group>
             <Form.Label style={{ fontSize: '40px' }}>Ray Ray's Word Bingo</Form.Label>
+            <p>I am sorry in advance for taking one of your jokes literally lmao</p>
           <Col sm={2}></Col>
          <Col sm={8}>
           <Form.Label style={{ fontSize: '30px' }}>Enter submitter name:</Form.Label>
