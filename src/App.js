3cdc870547593579f972c8a0c93d28114c9c5e05
Rei <rei@reiyua.lol>
2023-12-27 12:45:17 +0000

    Remove paragraph apologising in advance for the invention of this...

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ import { getFirestore,
         <Form>
           <Form.Group>
             <Form.Label style={{ fontSize: '40px' }}>Ray Ray's Word Bingo</Form.Label>
-            <p>I am sorry in advance Ash for taking one of your jokes literally lmao</p>
           <Col sm={2}></Col>
          <Col sm={8}>
           <Form.Label style={{ fontSize: '30px' }}>Enter submitter name:</Form.Label>
